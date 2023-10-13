94c1db2311566a8eab7ddf821647c6b7c7499cfb
ibrahimk4111 <ibrahimk4111@gmail.com>
2023-10-13 16:16:08 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import { App1 } from "./App.1";
+
+const App = () => {
+  return (
+    <>
+      <App1 />
+    </>
+  );
+};
+
+export default App;
