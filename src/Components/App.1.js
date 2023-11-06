7c9679e54e211bdb934453fb6bb2e47d377d3c83
MD. IBRAHIM KHALIL <58024873+ibrahimk4111@users.noreply.github.com>
2023-11-06 15:42:56 +0600

    title added

--- src/Components/App.1.js
+++ src/Components/App.1.js
@@ -5,7 +5,15 @@ export function App1() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <a
+    <a
+          className="App-link text-3xl"
+          href="https://github.com/ibrahimk4111"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Currency Converter
+        </a>   
+    <a
           className="App-link"
           href="https://github.com/ibrahimk4111"
           target="_blank"
